6ccb854f71a12fae5abd007e95373969e983917e
Troy Chryssos <github email address>
2016-09-01 09:32:39 -0400

    Wrote methods which use DOM and Jquery to create interactive pages

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $("p").click(function(){
+    alert("Hey!");
+  });
+}
 
-$(document).ready(function(){
+function frameIt(){
+  $("img").load(function(){
+    $(this).addClass("tasty");
+  });
+}
+
+function submitIt(){
+  $("form").submit(function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt(){
+  $("input").keydown(function(key){
+    if (key.which==71){
+      alert("You pressed 'G'!");
+    }
+  });
+}
+
+$("#typing").ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+
 
 });
